22859717334e6a195909ec33bfa83cb0dc54cc38
cwy <834415728@qq.com>
2021-12-17 17:32:21 +0800

    feat: add lerna

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # single-spa-lerna-demo
 single-spa+lerna demo
+
+子模块互相引用
+lerna add module-1 --scope=module-2
+
+全局安装
+lerna bootstrap
+
+单独跑项目
+lerna run --scope my-component test
---
+++ packages/module/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Button />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+import { Button } from "components"
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Button
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
